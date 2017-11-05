6ae66ecaa96e0ce35902ccd894f2243eb16496a5
Yogi D M <33394969+yogidm@users.noreply.github.com>
2017-11-05 21:24:26 +0700

    Delete SimpleThresholdingArray.py

--- SimpleThresholdingArray.py
+++
@@ -1,9 +0,0 @@
-#from https://stackoverflow.com/questions/32322281/numpy-matrix-binarization-using-only-one-expression
-
-import numpy as np
-np.random.seed(0)
-np.set_printoptions(precision=3)
-a=np.random.rand(4,4)
-print a
-a=(a>0.5).astype(np.int_)
-print a
